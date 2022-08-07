444bf4fa8e2448c0025ae258022b51a1a3a45007
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-08-07 10:43:36 +0100

    SQL-01: update scripts

---
+++ scripts/007_insert-data.sql
@@ -0,0 +1,7 @@
+-- Inserting into table 'employers
+INSERT INTO employers VALUES ('Betpawa', '1004 ESTATE, Victoria Island, Lagos', 150.13, true);
+INSERT INTO employers VALUES ('Betpawa', '1004 ESTATE, Victoria Island, Lagos', 150.13, true);
+
+
+-- Inserting into the table 'conversations'
+INSERT INTO conversations VALUES ('John Doe', 'Betpawa', 'Hello, I am looking for a job', now());
