1ca105e484eabdbb8cec23fea20a06a7c7739b01
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-08-07 22:24:59 +0100

    SQL-01: update scripts

--- onlineShopScripts/04_add-primary-key.sql
+++ onlineShopScripts/04_add-primary-key.sql
@@ -1,8 +1,8 @@
 ALTER TABLE products DROP id;
 
 INSERT INTO products (product_name, price, description, image, amount_in_stock)
-VALUES ('Pixel', '10', 'Description2', 'image1.jpg', 10);
+VALUES ('Acer', '15.89', 'Description3', 'image-acer.jpg', 15);
 
 ALTER TABLE products
-ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;
+ADD COLUMN id INT PRIMARY KEY AUTO_INCREMENT FIRST;
 
