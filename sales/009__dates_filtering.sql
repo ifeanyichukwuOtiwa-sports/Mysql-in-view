12984410fdaf8a8052d912d0dc59241707b921a5
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-08-19 23:34:11 +0100

    update scripts

--- sales/009__dates_filtering.sql
+++ sales/009__dates_filtering.sql
@@ -4,8 +4,10 @@ use sales_example;
 SELECT * FROM sales
 WHERE date_created > '2021-11-01' AND date_created < '2022-05-01';
 
+# BETWEEN is inclusive on both ends for ranges
 SELECT * FROM sales
 WHERE date_created BETWEEN '2021-11-02' AND '2022-04-30';
 
+# select ranges
 SELECT * FROM sales
 WHERE volume > 1000 AND volume < 100000;
--- sales/011__dates_calculation.sql
+++ sales/011__dates_calculation.sql
@@ -3,9 +3,18 @@ USE sales_example;
 SELECT * FROM sales
 WHERE date_fulfilled - date_created <= 5;
 
+SELECT id, date_created, date_fulfilled, date_fulfilled - date_created AS difference, customer_name, product_name, volume
+FROM sales
+WHERE date_fulfilled - date_created <= 5;
+
 SELECT * FROM sales
 WHERE date_fulfilled - date_created > 5;
 
 
 SELECT * FROM sales
+WHERE EXTRACT(DAY FROM date_fulfilled - date_created) >= 5;
+
+SELECT EXTRACT(DAY FROM date_fulfilled - date_created) AS difference, id, date_created, date_fulfilled, customer_name,
+       product_name,
+       volume, is_recurring, is_disputed FROM sales
 WHERE EXTRACT(DAY FROM date_fulfilled - date_created) >= 5;
