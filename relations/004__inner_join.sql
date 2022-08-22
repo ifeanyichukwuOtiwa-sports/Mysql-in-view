4f1cb12c299fae02a00e372d8bd79f1902829d37
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-08-22 19:03:16 +0100

    SQL-01: update scripts

--- relations/004__inner_join.sql
+++ relations/004__inner_join.sql
@@ -1,5 +1,13 @@
 USE relations;
 
-SELECT u.id, u.first_name, u.last_name, u.email, a.street_name, a.house_number, a.city_id
+SELECT u.id, u.first_name, u.last_name, u.email, a.street_name, a.house_number, c.city_name
 FROM users AS u
-INNER JOIN addresses a on a.id = u.address_id;
+INNER JOIN addresses a on u.address_id = a.id
+INNER JOIN cities c on a.city_id = c.id;
+
+
+SELECT u.id, CONCAT(u.first_name, ' ', u.last_name) AS user_name, u.email AS user_email,
+       CONCAT('No', ' ', a.house_number, ' ', a.street_name, ' ', c.city_name) AS user_address
+FROM users AS u
+         INNER JOIN addresses a on u.address_id = a.id
+         INNER JOIN cities c on a.city_id = c.id;
